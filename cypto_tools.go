3b09a7387ebdb7b99405535d89e026500062d089
heyong.wd <heyong.wd@alibaba-inc.com>
2018-05-30 14:42:34 +0800

    add rsa & base64

--- cypto_tools.go
+++ cypto_tools.go
@@ -2,12 +2,20 @@ package tools
 
 import (
 	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/pem"
+	"errors"
 )
 
 const (
 	base64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 )
+
+var coder = base64.NewEncoding(base64Table)
 
 /** md5 encrypt */
 func Md5Encrypt(data string) string {
@@ -16,4 +24,37 @@ func Md5Encrypt(data string) string {
 	cipherStr := md5Ctx.Sum(nil)
 	encryptStr := hex.EncodeToString(cipherStr)
 	return encryptStr
+}
+
+func Base64Encode(src []byte) []byte {
+	return []byte(coder.EncodeToString(src))
+}
+
+func RsaEncrypt(origData []byte, publicKey string) ([]byte, error) {
+	block, _ := pem.Decode([]byte(publicKey))
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
+}
+
+func RsaDecrypt(ciphertext []byte, privateKey string) ([]byte, error) {
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+}
+
+func Base64Decode(src []byte) ([]byte, error) {
+	return coder.DecodeString(string(src))
 }
